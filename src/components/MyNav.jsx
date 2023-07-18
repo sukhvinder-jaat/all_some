4faa5388bce7cb8e31204864f847f06b679169e6
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-18 09:56:54 +0530

    section

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
 
 export const MyNav = () => {
   return (
-    <div>MyNav</div>
-  )
-}
+    <div className="bg-black">
+      <Container>
+        <div>
+          <p className=" ff_inter text-white fs_xsm fw_500">
+            AllSome is your e-commerce operation team to auto-send your sold
+            orders.
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
+};
