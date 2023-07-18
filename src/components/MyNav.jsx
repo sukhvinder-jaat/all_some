04e5d5e52fd6fba827834b164570101a9374fe7a
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-18 10:36:16 +0530

    root

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -5,11 +5,15 @@ export const MyNav = () => {
   return (
     <div className="bg-black">
       <Container>
-        <div>
-          <p className=" ff_inter text-white fs_xsm fw_500">
+        <div className=" d-flex justify-content-between align-items-center py-3">
+          <p className="ff_inter text-white fs_xsm fw_500 mb-0">
             AllSome is your e-commerce operation team to auto-send your sold
             orders.
           </p>
+          <select>
+            <option value="1">ENG</option>
+            <option value="2">HIN</option>
+          </select>
         </div>
       </Container>
     </div>
