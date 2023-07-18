370e5474b126d0848889d55bf18f0a571818e0a7
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-07-18 10:20:43 +0530

    colors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { MyNav } from "./components/MyNav";
+import SellShoper from "./components/SellShoper";
 function App() {
   return (
     <div className="App">
-      <MyNav />
+      {/* <MyNav /> */}
+      <SellShoper/>
     </div>
   );
 }
--- src/components/SellShoper.jsx
+++ src/components/SellShoper.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Container } from 'react-bootstrap/lib/Tab';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap/esm';
+import { Container } from 'react-bootstrap';
 
 const SellShoper = () => {
   return (
@@ -7,10 +9,10 @@ const SellShoper = () => {
       <section className="py-5">
         <Container className="pt_37 pb_37">
           <Row>
-            <Col>
+            <Col lg={4}>
               <div className="sell_shoper_box_bg">
                 <div className="sell_shoper_padding">
-                  <p className=''>Sell to shoppers wherever they are</p>
+                  <p className="ff_kanit fw_600 fs_6xl  lh_normal mb-0">Sell to shoppers wherever they are</p>
                 </div>
               </div>
             </Col>
