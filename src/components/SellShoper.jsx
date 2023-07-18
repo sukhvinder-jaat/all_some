e82dd6758c43ee8de1d1c73db428a33b4a49a2cf
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-07-18 09:59:51 +0530

    sell

---
+++ src/components/SellShoper.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container } from 'react-bootstrap/lib/Tab';
+
+const SellShoper = () => {
+  return (
+    <>
+      <section className="py-5">
+        <Container className="pt_37 pb_37">
+          <Row>
+            <Col>
+              <div className="sell_shoper_box_bg">
+                <div className="sell_shoper_padding">
+                  <p className=''>Sell to shoppers wherever they are</p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+}
+
+export default SellShoper;
